7d40fca9211b38bd1809908acb0fe0ecc2426e91
VincentAlonso <vincent.j.alonso@gmail.com>
2024-12-02 16:59:29 -0800

    Setup the spdlog library in our engine

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -3,6 +3,7 @@
 //This file is only intended to be included by a client application
 
 #include "Hazel/Application.h"
+#include "Hazel/Log.h"
 
 // ----Entry Point----------------
 #include "Hazel/Entrypoint.h"
