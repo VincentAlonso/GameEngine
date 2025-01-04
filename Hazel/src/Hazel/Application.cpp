c48ad3a6c571f7363d4e8da1b2cc1674f6225d2e
VincentAlonso <vincent.j.alonso@gmail.com>
2025-01-04 15:02:37 -0800

    Made EventCallbacks for Window Events

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,17 +1,28 @@
 #include "hzpch.h"
 #include "Application.h"
 
-#include "Hazel/Events/ApplicationEvent.h"
 #include "Hazel/Log.h"
 
 namespace Hazel {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application() {
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application() {
 
+	}
+
+	void Application::OnEvent(Event& e) {
+
+		EventDispatcher dispatcher(e);
+
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		HZ_CORE_INFO("{0}", e);
 	}
 
 	void Application::Run() { //called after Entrypoint.h ->Run
@@ -19,5 +30,12 @@ namespace Hazel {
 		while (m_Running) {
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e) {
+
+		m_Running = false;
+
+		return true;
 	}
 }
--- Hazel/src/Hazel/Events/Event.h
+++ Hazel/src/Hazel/Events/Event.h
@@ -47,7 +47,7 @@ namespace Hazel {
 		{
 			return GetCategoryFlags() & category;
 		}
-	protected:
+	public:
 		bool m_Handled = false;
 	};
 
